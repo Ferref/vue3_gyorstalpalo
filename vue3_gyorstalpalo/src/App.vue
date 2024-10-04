15d5b02045d348346de9bcf59b4747fb3995e90d
Ferref <kovacsferenc026@gmail.com>
2024-10-04 12:14:49 +0200

    really basic login form

--- vue3_gyorstalpalo/src/App.vue
+++ vue3_gyorstalpalo/src/App.vue
@@ -1,27 +1,88 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup>import Welcome from './components/Welcome.vue';</script>
 
 <template>
-  <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/fat_cat.png" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <header></header>
+    <main>
+      <Welcome></Welcome>
+    </main>
+    <footer></footer>
+</div>
 </template>
 
-<style scoped>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Oswald&display=swap');
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
+:root {
+  /* Primary Colors */
+  --primary-color: #3498db;
+  --primary-dark: #2980b9;
+  --primary-light: #5dade2;
+  
+  /* Secondary Colors */
+  --secondary-color: #e74c3c;
+  --secondary-dark: #c0392b;
+  --secondary-light: #ec7063;
+  
+  /* Accent Colors */
+  --accent-color: #f1c40f;
+  --accent-dark: #f39c12;
+  --accent-light: #f7dc6f;
+
+  /* Neutral Colors */
+  --white: #ffffff;
+  --black: #000000;
+  --grey-light: #bdc3c7;
+  --grey-dark: #7f8c8d;
+
+  /* Background Colors */
+  --background-light: #ecf0f1;
+  --background-dark: #2c3e50;
+
+  /* Text Colors */
+  --text-primary: #2c3e50;
+  --text-secondary: #7f8c8d;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#app{
+  font-family: Oswald;
+}
+
+body{
+  background-color: var(--background-dark);
+}
+
+.display-flex {
+  display: flex;
+}
+
+.flex-column {
+  flex-direction: column;
+}
+
+.flex-center{
+  justify-content: center;
+  align-items: center;
+}
+
+.title{
+  font-size: 3rem;
 }
 </style>
---
+++ vue3_gyorstalpalo/src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div class="container">
+    <div class="display-flex flex-row">
+      <h1 class="title">task$</h1>
+      <h2>Meow Meow Blyat</h2>
+      <img src="../assets/fat_cat.png" class="logo vue" alt="Vue logo" />
+    </div>
+
+    <form class="display-flex flex-column">
+        <input type="email" placeholder="Email" />
+        <input type = "password" placeholder="Jelszo"/>
+        <button>Login</button>
+    </form>
+  </div>
+</template>
+  
+<style scoped>
+/* h1 {
+    background-color: var(--background-light);
+} */
+</style>
